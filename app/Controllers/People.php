943c2439c7e11c7cb1ff4be1f8f9cc2819950d3c
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-11-29 23:28:27 -0600

    config Controllers/People

---
+++ app/Controllers/People.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\Gender;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class People extends BaseController
+{
+    public function index(): string
+    {
+        $peopleModel = new \App\Models\People();
+        $data = [
+            'people' => $data['people'] = $peopleModel
+                ->join('gender', 'people.gender_id = gender.id', 'left')
+                ->select('people.*, gender.name as gender_name')
+                ->findAll(),
+            'genders' => (new Gender())->findAll()
+        ];
+
+        return view('people/index', $data);
+    }
+
+    public function createOrEdit(): ResponseInterface
+    {
+        $id = $this->request->getPost('id');
+        $name = $this->request->getPost('name');
+        $last_name = $this->request->getPost('last_name');
+        $middle_name = $this->request->getPost('middle_name');
+        $age = $this->request->getPost('age');
+        $gender_id = $this->request->getPost('gender_id');
+
+        $peopleModel = new \App\Models\People();
+        $peopleModel->callProcedureSaveOrUpdatePerson($id, $name, $last_name, $middle_name, $age, $gender_id);
+        return $this->response->setJSON(['status' => true, 'message' => 'Datos guardados con éxito']);
+    }
+
+    public function show($id = null): ResponseInterface
+    {
+        $peopleModel = new \App\Models\People();
+
+        $people = $peopleModel->join('gender', 'people.gender_id = gender.id', 'left')
+            ->select('people.*, gender.name as gender_name')->find($id);
+
+        return $this->response->setJSON(['status' => isset($people), 'data' => $people]);
+    }
+}
